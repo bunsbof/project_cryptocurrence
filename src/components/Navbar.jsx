ebc71f13747e5a3c4e61e1ffe2a4d59294b938f7
bunsbof <nguyenminhcuong2000@gmail.com>
2023-02-16 00:49:40 +0700

    #2023/02/16 cuong - initial currency project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FunctionOutlined, MenuFoldOutlined } from '@ant-design/icons'
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
