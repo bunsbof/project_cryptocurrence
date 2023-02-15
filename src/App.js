417c08dd4402e272179c593394ce9f8947edc115
bunsbof <nguyenminhcuong2000@gmail.com>
2023-02-16 01:30:41 +0700

    #2023/02/16 cuong - creating Navbar code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FunctionOutlined, MenuFoldOutlined } from '@ant-design/icons'
+import icon from '../assets/cryptocurrency.png'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='nav-container'>
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+            <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button className='menu-control-container'></Button> */}
+      </div>
     </div>
   )
 }
